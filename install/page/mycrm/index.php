380e285e24c99c5a470a28aae978c5bf5e1249cc
foton <mr.kamischin@gmail.com>
2022-12-28 21:19:18 +0300

    requestindex

--- install/page/mycrm/index.php
+++ install/page/mycrm/index.php
@@ -5,12 +5,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 <?$APPLICATION->SetTitle('CRM SYSTEMS');?>
 <?php
 	$APPLICATION->IncludeComponent(
-	  "mycrm:crm","",array(
-		'LID'=>'s1',
-		'FIELDS'=>$field[$table],
-		'TABLE'=>$table,
-		'ID'=>$id
-		)
+	  "mycrm:crm","",array()
 	  );
 ?>
 <div class="ajaxBike"></div>
