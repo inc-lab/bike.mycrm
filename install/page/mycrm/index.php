5df66cd78a478f19327691d3a38ea0ad0b3395f8
foton <mr.kamischin@gmail.com>
2023-01-13 17:06:09 +0300

    requestindex2

--- install/page/mycrm/index.php
+++ install/page/mycrm/index.php
@@ -7,6 +7,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 	$APPLICATION->IncludeComponent(
 	  "mycrm:crm","",array()
 	  );
+	  
 ?>
 <div class="ajaxBike"></div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
